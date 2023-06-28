9cef5eda08e7d6d0b8dc48e10cdd57f92108d024
Julian Molineris <110922176+julianmolinerisit@users.noreply.github.com>
2023-06-28 13:49:37 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 // Configuramos la conexión a la base de datos
 const db = new Sequelize('u742358870_database_app', 'u742358870_julian', 'Aa27462582', {
-  host: ' srv719.hstgr.io ',
+  host: '185.211.7.205',
   dialect: 'mysql',
 });
 
